b90df0c0cf664e4752d27812f2afc57625c05101
notminecraftr <shahzad21531@gmail.com>
2026-07-05 08:26:38 -0400

    Adds columns to messages and channel participants table

--- backend/priv/repo/migrations/20260705073103_channel_participants.exs
+++ backend/priv/repo/migrations/20260705073103_channel_participants.exs
@@ -3,7 +3,8 @@ defmodule Backend.Repo.Migrations.ChannelParticipants do
 
   def change do
     create table(:channel_participants, primary_key: false) do
-      add :channel_id, references(:channels, type: :binary_id, on_delete: :delete_all), primary_key: true, null: false
+      add :id, :binary_id, primary_key: true, null: false
+      add :channel_id, references(:channels, type: :binary_id, on_delete: :delete_all), null: false
       add :user_id, references(:users, type: :binary_id, on_delete: :delete_all), null: false
       add :role, :string
       add :left_at, :timestamptz
--- backend/priv/repo/migrations/20260705074442_messages.exs
+++ backend/priv/repo/migrations/20260705074442_messages.exs
@@ -1,13 +1,14 @@
 defmodule Backend.Repo.Migrations.Messages do
   use Ecto.Migration
 
-  def change do
+  def change do #add types later
     create table(:messages, primary_key: false) do
       add :id, :binary_id, primary_key: true, null: false
       add :channel_id, references(:channels, type: :binary_id, on_delete: :delete_all), null: false
       add :author_id, references(:users, type: :binary_id, on_delete: :nothing), null: false
       add :parent_message_id, references(:messages, type: :binary_id)
       add :content, :text
+      add :type, :string
       add :metadata, :string #Not sure the type of metadata
       add :deleted_at, :timestamptz
 
